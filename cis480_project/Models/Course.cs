627be36aa01e85b76877179c5b0c0f6c2517f4ae
Bill Jordan <bill@billjordan.info>
2015-05-19 11:02:04 -0400

    Fix Courses' CampusId FK

--- cis480_project/Models/Course.cs
+++ cis480_project/Models/Course.cs
@@ -16,5 +16,9 @@ namespace cis480_project.Models {
         public string Name { get; set; }
         public string Designator { get; set; }
 
+        public int CampusId { get; set; }
+        [ForeignKey("CampusId")]
+        public Campus Campus { get; set; }
+
     }
 }
